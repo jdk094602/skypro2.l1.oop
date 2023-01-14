5a3bbbabb7c28a6af365f8698df68003cc4f42b6
Gen <jdk094602@gmail.com>
2023-01-15 01:42:18 +0300

    3rd

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Car[] car = new Car[7];
+        car[0] = new Car("Lada","Granta",1.7F,"желтый",2015,"Россия"  );
+        car[1] = new Car("Audi","A8 50 L TDI quattro",3F,"чёрный",2020,"Германия"  );
+        car[2] = new Car("BMW","Z8",3F,"чёрный",2021,"Германия"  );
+        car[3] = new Car("KIA","Sportage",2.4F,"красный",2018,"Южная Корея"  );
+        car[4] = new Car("Hyundai","Avante",1.6F,"оранжевый",2016,"Южная Корея"  );
+        car[5] = new Car("Hewlett Packard","ColorJet",0,"",1998,"США"  );
+        car[6] = new Car("Epson","FX - 900 ",0,"",1999,""  );
+
+        System.out.println(Arrays.toString(car));
     }
 }
